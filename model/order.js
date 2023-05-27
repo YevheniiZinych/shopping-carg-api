a48addc8ef7493fd31c815f8b23df035d1f28b87
ZEugenii <zinich911@gmail.com>
2023-05-27 11:26:47 +0300

    create restaurant and order schema

---
+++ model/order.js
@@ -0,0 +1,82 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const { handleMongooseError } = require("../helpers");
+
+const orderSchema = new Schema(
+  {
+    cart: [
+      {
+        id: {
+          type: String,
+        },
+        name: {
+          type: String,
+        },
+        prise: {
+          type: String,
+        },
+        amount: {
+          type: String,
+        },
+      },
+    ],
+    data: {
+      address: {
+        type: String,
+        required: [true, "Set address for delivery information"],
+      },
+      email: {
+        type: String,
+        required: [true, "Set email for delivery information"],
+      },
+      name: {
+        type: String,
+        required: [true, "Set name for delivery information"],
+      },
+      phone: {
+        type: String,
+        required: [true, "Set phone for delivery information"],
+      },
+    },
+    prise: {
+      type: String,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+shopSchema.post("save", handleMongooseError);
+
+const Order = model("orders", orderSchema);
+
+const addOrderSchema = Joi.object({
+  cart: Joi.array().items(
+    Joi.object().keys({
+      name: Joi.string().required(),
+      id: Joi.string(),
+      price: Joi.string().required(),
+      amount: Joi.string().required(),
+    })
+  ),
+  data: Joi.object().keys({
+    name: Joi.string()
+      .pattern(
+        new RegExp("^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$")
+      )
+      .min(3)
+      .max(30)
+      .required(),
+    email: Joi.string().email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    }),
+    phone: Joi.string().min(10).max(12).required(),
+    address: Joi.string().required(),
+  }),
+});
+
+module.exports = {
+  addOrderSchema,
+  Order,
+};
